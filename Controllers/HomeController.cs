83ff9a570114bb85ab5aaae32ac45f6a0c228fa9
Mert Araz <mertoreyiz1907@gmail.com>
2024-08-13 17:53:10 +0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BestCloudCase_API.Controllers.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        // GET /
+        [HttpGet("/")]
+        public IActionResult GetMessage()
+        {
+            return Ok(new { msg = "BC4M" });
+        }
+
+        // GET /health
+        [HttpGet("health")]
+        public IActionResult GetHealthStatus()
+        {
+            return Ok(new { status = "Healthy" });
+        }
+
+        // POST /
+        [HttpPost("/")]
+        public IActionResult EchoData([FromBody] object data)
+        {
+            return Ok(data);
+        }
+    }
+}
